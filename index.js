d97d01a4a33970ac0f50c7a8591dc95e1acc8e5d
MegarajSF4781 <megaraj.srinivasan@syncfusion.com>
2025-04-24 16:34:56 +0530

    Commit

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-ej.treegrid.TreeGrid.Inject(ej.treegrid.Page,ej.treegrid.Reorder,ej.treegrid.Freeze,
-    ej.treegrid.ColumnChooser,ej.treegrid.ColumnMenu,ej.treegrid.RowDD,ej.treegrid.ContextMenu,
-    ej.treegrid.Toolbar,ej.treegrid.Resize,ej.treegrid.Filter);
+ej.treegrid.TreeGrid.Inject(ej.treegrid.Page, ej.treegrid.Reorder, ej.treegrid.Freeze,
+    ej.treegrid.ColumnChooser, ej.treegrid.ColumnMenu, ej.treegrid.RowDD, ej.treegrid.ContextMenu,
+    ej.treegrid.Toolbar, ej.treegrid.Resize, ej.treegrid.Filter);
 var virtualData = [];
-var dataSource = function() {
+var dataSource = function () {
     var parent = -1;
-    var names = ['VINET', 'TOMSP', 'HANAR', 'VICTE', 'SUPRD', 'HANAR', 'CHOPS', 'RICSU', 'WELLI','HILAA', 'ERNSH', 'CENTC',
-'OTTIK', 'QUEDE', 'RATTC', 'ERNSH', 'FOLKO', 'BLONP', 'WARTH', 'FRANK', 'GROSR', 'WHITC', 'WARTH', 'SPLIR', 'RATTC', 'QUICK', 'VINET',
-'MAGAA', 'TORTU', 'MORGK', 'BERGS', 'LEHMS', 'BERGS', 'ROMEY', 'ROMEY', 'LILAS', 'LEHMS', 'QUICK', 'QUICK', 'RICAR', 'REGGC', 'BSBEV',
-'COMMI', 'QUEDE', 'TRADH', 'TORTU', 'RATTC', 'VINET', 'LILAS', 'BLONP', 'HUNGO', 'RICAR', 'MAGAA', 'WANDK', 'SUPRD', 'GODOS', 'TORTU',
-'OLDWO', 'ROMEY', 'LONEP', 'ANATR', 'HUNGO', 'THEBI', 'DUMON', 'WANDK', 'QUICK', 'RATTC', 'ISLAT', 'RATTC', 'LONEP', 'ISLAT', 'TORTU',
-'WARTH', 'ISLAT', 'PERIC', 'KOENE', 'SAVEA', 'KOENE', 'BOLID', 'FOLKO', 'FURIB', 'SPLIR', 'LILAS', 'BONAP', 'MEREP', 'WARTH', 'VICTE',
-'HUNGO', 'PRINI', 'FRANK', 'OLDWO', 'MEREP', 'BONAP', 'SIMOB', 'FRANK', 'LEHMS', 'WHITC', 'QUICK', 'RATTC', 'FAMIA'];
+    var names = ['VINET', 'TOMSP', 'HANAR', 'VICTE', 'SUPRD', 'HANAR', 'CHOPS', 'RICSU', 'WELLI', 'HILAA', 'ERNSH', 'CENTC',
+        'OTTIK', 'QUEDE', 'RATTC', 'ERNSH', 'FOLKO', 'BLONP', 'WARTH', 'FRANK', 'GROSR', 'WHITC', 'WARTH', 'SPLIR', 'RATTC', 'QUICK', 'VINET',
+        'MAGAA', 'TORTU', 'MORGK', 'BERGS', 'LEHMS', 'BERGS', 'ROMEY', 'ROMEY', 'LILAS', 'LEHMS', 'QUICK', 'QUICK', 'RICAR', 'REGGC', 'BSBEV',
+        'COMMI', 'QUEDE', 'TRADH', 'TORTU', 'RATTC', 'VINET', 'LILAS', 'BLONP', 'HUNGO', 'RICAR', 'MAGAA', 'WANDK', 'SUPRD', 'GODOS', 'TORTU',
+        'OLDWO', 'ROMEY', 'LONEP', 'ANATR', 'HUNGO', 'THEBI', 'DUMON', 'WANDK', 'QUICK', 'RATTC', 'ISLAT', 'RATTC', 'LONEP', 'ISLAT', 'TORTU',
+        'WARTH', 'ISLAT', 'PERIC', 'KOENE', 'SAVEA', 'KOENE', 'BOLID', 'FOLKO', 'FURIB', 'SPLIR', 'LILAS', 'BONAP', 'MEREP', 'WARTH', 'VICTE',
+        'HUNGO', 'PRINI', 'FRANK', 'OLDWO', 'MEREP', 'BONAP', 'SIMOB', 'FRANK', 'LEHMS', 'WHITC', 'QUICK', 'RATTC', 'FAMIA'];
     for (var i = 0; i < 50000; i++) {
         if (i % 5 === 0) {
             parent = i;
         }
         if (i % 5 !== 0) {
-            var num = isNaN((virtualData.length % parent)- 1) ?  0 : (virtualData.length % parent) - 1;
+            var num = isNaN((virtualData.length % parent) - 1) ? 0 : (virtualData.length % parent) - 1;
             virtualData[num].Crew.push({
                 'TaskID': i + 1,
                 'FIELD1': names[Math.floor(Math.random() * names.length)],
@@ -92,42 +92,115 @@ dataSource();
 var tree = new ej.treegrid.TreeGrid({
     dataSource: sampleData,
     childMapping: 'subtasks',
-    
+
     allowPaging: true,
-    pageSettings: {pageSize: 10},
+    pageSettings: { pageSize: 10 },
     treeColumnIndex: 1,
-    allowResizing:true,
-showColumnChooser: true,
-allowRowDragAndDrop:true,
-allowFiltering: true,
-filterSettings: { type: 'Menu'},
-editSettings : {allowAdding:true,allowDeleting:true,allowEditing:true},
-enableStickyHeader:true,
-toolbar: ['ColumnChooser','Add','Delete'],
-allowReordering : true,
-showColumnMenu: true,
-contextMenuItems: ['AutoFit', 'AutoFitAll', 'SortAscending', 'SortDescending',
-    'Edit', 'Delete', 'Save', 'Cancel',
-   'PdfExport', 'ExcelExport', 'CsvExport', 'FirstPage', 'PrevPage',
-   'LastPage', 'NextPage','Indent', 'Outdent'],
-   
-   frozenRows:2,
-columns: [
-    { field: 'taskID', headerText: 'Task ID', width: 70, textAlign: 'Right',isPrimaryKey :true },
-    { field: 'taskName', headerText: 'Task Name', width: 200, textAlign: 'Left' },
-    { field: 'startDate', headerText: 'Start Date', width: 90, textAlign: 'Right', type: 'date', format: 'yMd' },
-    { field: 'endDate', headerText: 'End Date', width: 90, textAlign: 'Right', type: 'date', format: 'yMd' },
-    { field: 'duration', headerText: 'Duration', width: 80, textAlign: 'Right' },
-    { field: 'progress', headerText: 'Progress', width: 80, textAlign: 'Right' },
-    { field: 'priority', headerText: 'Priority', width: 90 }
-]
-    });
-    tree.appendTo('#TreeGrid');
-    let select = new ej.buttons.Button();
-    select.appendTo('#sticky');
-    let a  = 1;
-    document.getElementById('sticky').onclick = () => {
-      
-        tree.enableStickyHeader= tree.enableStickyHeader === true ? false : true;
-      
-    }
+    allowResizing: true,
+    showColumnChooser: true,
+    allowRowDragAndDrop: true,
+    allowFiltering: true,
+    filterSettings: { type: 'Menu' },
+    editSettings: { allowAdding: true, allowDeleting: true, allowEditing: true },
+    enableStickyHeader: true,
+    toolbar: ['ColumnChooser', 'Add', 'Delete'],
+    allowReordering: true,
+    showColumnMenu: true,
+    contextMenuItems: ['AutoFit', 'AutoFitAll', 'SortAscending', 'SortDescending',
+        'Edit', 'Delete', 'Save', 'Cancel',
+        'PdfExport', 'ExcelExport', 'CsvExport', 'FirstPage', 'PrevPage',
+        'LastPage', 'NextPage', 'Indent', 'Outdent'],
+
+    frozenRows: 2,
+    columns: [
+        { field: 'taskID', headerText: 'Task ID', width: 70, textAlign: 'Right', isPrimaryKey: true },
+        { field: 'taskName', headerText: 'Task Name', width: 200, textAlign: 'Left' },
+        { field: 'startDate', headerText: 'Start Date', width: 90, textAlign: 'Right', type: 'date', format: 'yMd' },
+        { field: 'endDate', headerText: 'End Date', width: 90, textAlign: 'Right', type: 'date', format: 'yMd' },
+        { field: 'duration', headerText: 'Duration', width: 80, textAlign: 'Right' },
+        { field: 'progress', headerText: 'Progress', width: 80, textAlign: 'Right' },
+        { field: 'priority', headerText: 'Priority', width: 90 }
+    ]
+});
+tree.appendTo('#TreeGrid');
+let select = new ej.buttons.Button();
+select.appendTo('#sticky');
+let a = 1;
+document.getElementById('sticky').onclick = () => {
+
+    tree.enableStickyHeader = tree.enableStickyHeader === true ? false : true;
+    tree2.enableStickyHeader = tree2.enableStickyHeader === true ? false : true;
+    tree3.enableStickyHeader = tree3.enableStickyHeader === true ? false : true;
+
+}
+
+var tree2 = new ej.treegrid.TreeGrid({
+    dataSource: sampleData,
+    childMapping: 'subtasks',
+
+    allowPaging: true,
+    pageSettings: { pageSize: 10 },
+    treeColumnIndex: 1,
+    allowResizing: true,
+    showColumnChooser: true,
+    allowRowDragAndDrop: true,
+    allowFiltering: true,
+    filterSettings: { type: 'Menu' },
+    editSettings: { allowAdding: true, allowDeleting: true, allowEditing: true },
+    enableStickyHeader: true,
+    toolbar: ['ColumnChooser', 'Add', 'Delete'],
+    allowReordering: true,
+    showColumnMenu: true,
+    contextMenuItems: ['AutoFit', 'AutoFitAll', 'SortAscending', 'SortDescending',
+        'Edit', 'Delete', 'Save', 'Cancel',
+        'PdfExport', 'ExcelExport', 'CsvExport', 'FirstPage', 'PrevPage',
+        'LastPage', 'NextPage', 'Indent', 'Outdent'],
+
+    frozenRows: 2,
+    columns: [
+        { field: 'taskID', headerText: 'Task ID', width: 70, textAlign: 'Right', isPrimaryKey: true },
+        { field: 'taskName', headerText: 'Task Name', width: 200, textAlign: 'Left' },
+        { field: 'startDate', headerText: 'Start Date', width: 90, textAlign: 'Right', type: 'date', format: 'yMd' },
+        { field: 'endDate', headerText: 'End Date', width: 90, textAlign: 'Right', type: 'date', format: 'yMd' },
+        { field: 'duration', headerText: 'Duration', width: 80, textAlign: 'Right' },
+        { field: 'progress', headerText: 'Progress', width: 80, textAlign: 'Right' },
+        { field: 'priority', headerText: 'Priority', width: 90 }
+    ]
+});
+tree2.appendTo('#TreeGrid2');
+
+var tree3 = new ej.treegrid.TreeGrid({
+    dataSource: sampleData,
+    childMapping: 'subtasks',
+    height:400,
+    // allowPaging: true,
+    pageSettings: { pageSize: 20 },
+    treeColumnIndex: 1,
+    allowResizing: true,
+    showColumnChooser: true,
+    allowRowDragAndDrop: true,
+    allowFiltering: true,
+    filterSettings: { type: 'Menu' },
+    editSettings: { allowAdding: true, allowDeleting: true, allowEditing: true },
+    enableStickyHeader: true,
+    toolbar: ['ColumnChooser', 'Add', 'Delete'],
+    allowReordering: true,
+    showColumnMenu: true,
+    contextMenuItems: ['AutoFit', 'AutoFitAll', 'SortAscending', 'SortDescending',
+        'Edit', 'Delete', 'Save', 'Cancel',
+        'PdfExport', 'ExcelExport', 'CsvExport', 'FirstPage', 'PrevPage',
+        'LastPage', 'NextPage', 'Indent', 'Outdent'],
+
+    frozenRows: 2,
+    columns: [
+        { field: 'taskID', headerText: 'Task ID', width: 70, textAlign: 'Right', isPrimaryKey: true },
+        { field: 'taskName', headerText: 'Task Name', width: 200, textAlign: 'Left' },
+        { field: 'startDate', headerText: 'Start Date', width: 90, textAlign: 'Right', type: 'date', format: 'yMd' },
+        { field: 'endDate', headerText: 'End Date', width: 90, textAlign: 'Right', type: 'date', format: 'yMd' },
+        { field: 'duration', headerText: 'Duration', width: 80, textAlign: 'Right' },
+        { field: 'progress', headerText: 'Progress', width: 80, textAlign: 'Right' },
+        { field: 'priority', headerText: 'Priority', width: 90 }
+    ]
+});
+
+tree3.appendTo('#TreeGrid3');
